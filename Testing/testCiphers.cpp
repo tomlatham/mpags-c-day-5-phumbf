970991d043433afb6eac7854d8a2e57591696dd2
Edward <E.J.Millard@warwick.ac.uk>
2016-11-21 23:40:30 +0000

    Homework

--- Testing/testCiphers.cpp
+++ Testing/testCiphers.cpp
@@ -6,39 +6,34 @@
 #include "PlayfairCipher.hpp"
 #include "VigenereCipher.hpp"
 #include "Cipher.hpp"
+#include "CipherType.hpp"
+#include "FactoryFunction.hpp"
+#include <iostream>
+#include <memory>
+#include <vector>
 
 
-bool testCipher( const Cipher& cipher, const CipherMode mode, const std::string& inputText, const std::string& outputText)
-{
- return cipher.applyCipher(inputText, mode) == outputText;
+TEST_CASE("Caesar, Playfair and Vigenere encryption and decryption tests"){
+    
+    // Declare strings for the keys
+    std::string key1{"10"};
+    std::string key2{"hello"};
+    std::string key3{"key"};
+    
+    // Setup collection of Caesar, Playfair and Vigenere 
+    std::vector<std::unique_ptr<Cipher>> testnumber;
+    testnumber.push_back(cipherFactory(CipherType::Caesar, key1));
+    testnumber.push_back(cipherFactory(CipherType::Playfair, key2));
+    testnumber.push_back(cipherFactory(CipherType::Vigenere, key3));  
+    
+    // Strings to test with, i.e. encryption and decryption
+    std::vector<std::string> encryption = {"HELLOWORLD", "THEDOGSATONTHEMATS", "HELLOWORLD"};
+    std::vector<std::string> decryption = {"ROVVYGYBVN", "QOLCAFULYFTYELPLUT", "RIJVSUYVJN"};
+    
+    // Loop which checks whether encryption and decryption give the correct results
+    for(size_t i{0}; i<testnumber.size(); i++){
+        REQUIRE( testnumber[i]->applyCipher(encryption[i],CipherMode::Encrypt) == decryption[i]);
+        REQUIRE( testnumber[i]->applyCipher(decryption[i],CipherMode::Decrypt) == encryption[i]);
+}
 }
 
-TEST_CASE("VigenereCipher encryption", "[vigenere]") {
-  VigenereCipher vc{"key"};
-  REQUIRE( testCipher(vc, CipherMode::Encrypt, "HELLOWORLD", "RIJVSUYVJN" ));
-}
-
-TEST_CASE("VigenereCipher decryption", "[vigenere]") {
-  VigenereCipher vc{"key"};
-  REQUIRE( testCipher(vc, CipherMode::Decrypt,"RIJVSUYVJN", "HELLOWORLD" ));
-}
-
-TEST_CASE("Caesar Cipher encryption", "[caesar]") {
-  CaesarCipher cc{10};
-  REQUIRE( testCipher(cc, CipherMode::Encrypt, "HELLOWORLD", "ROVVYGYBVN"));
-}
-  
-TEST_CASE("Caesar Cipher decryption", "[caesar]") {
-  CaesarCipher cc{10};
-  REQUIRE( testCipher(cc, CipherMode::Decrypt,"ROVVYGYBVN" ,"HELLOWORLD" ));
-}
-  
-TEST_CASE("Playfair Cipher encryption", "[playfair]") {
-  PlayfairCipher pc{"hello"};
-  REQUIRE( testCipher(pc, CipherMode::Encrypt, "BOBISSOMESORTOFJUNIORCOMPLEXXENOPHONEONEZEROTHING", "FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA"));
-}
-  
-TEST_CASE("Playfair Cipher decryption", "[playfair]") {
-  PlayfairCipher pc{"hello"};
-  REQUIRE( testCipher(pc, CipherMode::Decrypt, "FHIQXLTLKLTLSUFNPQPKETFENIOLVSWLTFIAFTLAKOWATEQOKPPA", "BOBISXSOMESORTOFIUNIORCOMPLEXQXENOPHONEONEZEROTHINGZ"));
-}  
